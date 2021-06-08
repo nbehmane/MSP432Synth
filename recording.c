38628ee452abaa9547a26c052c4bec16466f4c3a
nbehmane <nbehmane@calpoly.edu>
2021-06-08 14:52:04 -0700

    Added comments to recording.c

--- recording.c
+++ recording.c
@@ -6,6 +6,17 @@
  */
 #include "recording.h"
 
+/*
+ * INPUT| uint8_t period    : a note value or a user command
+ *        Note *notes       : an array to store notes
+ *        uint8_t note_index: an index in notes to store a note
+ *
+ * OUTPUT| void
+ *
+ * Description: stores a note in notes to be played back.
+ *  Stores a rough estimate of the total time a user pressed that note.
+ *  Clears BIT0 to indicate that a note has been recorded.
+ */
 void recording(uint8_t period, Note *notes, uint8_t note_index)
 {
     if (RECORDING & period != PLAYBACK_BTN)
@@ -16,13 +27,24 @@ void recording(uint8_t period, Note *notes, uint8_t note_index)
     clear_flag(BIT0);
 }
 
+/*
+ * INPUT| Note *notes        : An array of recorded notes.
+ *        uint8_t wave_select: a value to represent the wave that
+ *          should be used for playback.
+ * OUTPUT| void.
+ *
+ * Description: Plays back through the notes.
+ */
 void playback(Note *notes, uint8_t wave_select)
 {
     uint16_t playback_index = 0;
     uint32_t playback_duration = 0;
+
+    // Indicate that playback is happening on the LCD
     delay_us(1000);
     writeStringOffset("PLAYING BACK", 12, SECONDLINE);
 
+    // Playback loop
     for (playback_index = 0; playback_index < NOTE_LEN; playback_index++)
     {
         for (playback_duration = 0;
@@ -30,14 +52,21 @@ void playback(Note *notes, uint8_t wave_select)
              playback_duration++)
         {
             delay_us(notes[playback_index].period);
-            transmit(get_next_value(wave_select));
+            transmit(get_next_value(wave_select));ss
         }
+        // delay a break between the notes.
         delay_us(BREAK_NOTE);
+
+        // transmit nothing after taking the break.
         transmit(0);
     }
+
+    // Indicate that the playback is finished.
     writeStringOffset("DONE            ", 17, SECONDLINE);
 }
 
+
+// Free the old recording, and initialize new memory for a new recording.
 void free_recording(Note *notes, uint8_t *note_index, uint8_t *period)
 {
     free(notes);
