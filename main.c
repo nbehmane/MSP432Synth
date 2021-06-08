328cb121b71083fbe382b73db3a95649bee7337b
nbehmane <nbehmane@calpoly.edu>
2021-06-08 14:01:39 -0700

    Final version of project

--- main.c
+++ main.c
@@ -11,16 +11,9 @@
 #include "stdlib.h"
 #include "recording.h"
 #define SWITCH_WAVE ('#')
-
-
-
-
 /**
  * main.c
  */
-
-
-
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
